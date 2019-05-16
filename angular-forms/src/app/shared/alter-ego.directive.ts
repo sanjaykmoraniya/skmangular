46e6b98edf2d1687d40a3effb2bacd04a873ad9f
sanjaykmoraniya <smoraniy@BLR.ALLEGISINDIA.COM>
2019-05-16 17:48:26 +0530

    Removed unwanted code

--- angular-forms/src/app/shared/alter-ego.directive.ts
+++ angular-forms/src/app/shared/alter-ego.directive.ts
@@ -40,10 +40,3 @@ export class UniqueAlterEgoValidatorDirective {
     this.validator.validate(control);
   }
 }
-
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
